7fbf561f61979e067c6991d9e63914f6585ed5ac
SGCMarkus <markusornik@gmail.com>
2022-10-18 22:51:42 +0200

    AGMIPC@1.0-impl/libagm: exclude from OSS builds

--- ipc/HwBinders/agm_ipc_service/Android.mk
+++ ipc/HwBinders/agm_ipc_service/Android.mk
@@ -1,4 +1,6 @@
 LOCAL_PATH := $(call my-dir)
+
+ifneq ($(QCPATH),)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := vendor.qti.hardware.AGMIPC@1.0-impl
@@ -21,6 +23,8 @@ LOCAL_SHARED_LIBRARIES := \
     libagm
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
 
 include $(CLEAR_VARS)
 
